d63b3a6300c4cf3be32b32c5a6c418e762297ff0
Emily-MM <65458318+Emily-MM@users.noreply.github.com>
2024-09-10 19:09:15 -0500

    Add files via upload

---
+++ nutri-api/src/main/java/com/hampcode/api/UserController.java
@@ -0,0 +1,55 @@
+package com.hampcode.api;
+
+import com.hampcode.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.hampcode.model.entity.User;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> list(){
+        List<User> users = UserService.findAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user){
+        User u = userService.create(user);
+        return new ResponseEntity<>(u, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user){
+        if (userService.checkCredentials(user.getUsername(),user.getPassword())){
+            return new ResponseEntity<>('Login successful', HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>('Invalid login', HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> get(@PathVariable Integer id){
+        User u = userService.getOne(id);
+        return new ResponseEntity<>(u, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Integer id, @RequestBody User user){
+        User u = userService.update(id, user);
+        return new ResponseEntity<>(u, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> delete(@PathVariable Integer id){
+        userService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
