dcb1146e6e19763e5c68a9e657f4f673344a42ac
Paula215 <u202115844@upc.edu.pe>
2024-09-12 19:43:20 -0500

    update appointment

--- nutri-api/src/main/java/com/hampcode/model/entity/Appointment.java
+++ nutri-api/src/main/java/com/hampcode/model/entity/Appointment.java
@@ -14,28 +14,15 @@ public class Appointment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name="activate", nullable = false)
-    private Boolean activate_appointment;
-
-    @OnetoOne
-    @JoinColumn(name = "id_doctor", referencedColumnName = "id"
-            ,foreignKey = @ForeignKey(name = "FK_id_doctor"))
-    private Doctor doctor;
-
     @OnetoOne
     @JoinColumn(name = "id_history", referencedColumnName = "id"
             ,foreignKey = @ForeignKey(name = "FK_id_history"))
     private History history;
 
     @OnetoOne
-    @JoinColumn(name = "id_date", referencedColumnName = "id"
+    @JoinColumn(name = "id_availability", referencedColumnName = "id"
             ,foreignKey = @ForeignKey(name = "FK_id_date"))
-    private Availability id_date;
-
-    @OnetoOne
-    @JoinColumn(name = "id_time", referencedColumnName = "id"
-            ,foreignKey = @ForeignKey(name = "FK_id_time"))
-    private Availability id_time;
+    private Availability id_availability;
 
 }
 
