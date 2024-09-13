8fa46bb9b99b9bb23d03e91e80d5117501cb7761
Paula215 <95729131+Paula215@users.noreply.github.com>
2024-09-13 09:57:38 -0500

    Revert "Pauz branch"

--- nutri-api/src/test/java/com/hampcode/model/repository/MealRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hampcode.model.repository;
-
-import com.hampcode.model.entity.Meal;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MealRepository extends JpaRepository<Meal, Integer> {
-}
--- repository/ExerciseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ayrtonto.nutriapi.repository;
-
-import com.ayrtonto.nutriapi.model.entity.Exercise;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExerciseRepository extends JpaRepository<Exercise, Integer> {
-}
