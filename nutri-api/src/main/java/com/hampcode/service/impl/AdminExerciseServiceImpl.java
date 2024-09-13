c619660a436b3febed95aced6ee7fdd27183718b
Emily-MM <65458318+Emily-MM@users.noreply.github.com>
2024-09-12 20:44:42 -0500

    Add files via upload

---
+++ nutri-api/src/main/java/com/hampcode/service/impl/AdminExerciseServiceImpl.java
@@ -0,0 +1,69 @@
+package com.ayrtonto.nutriapi.service.impl;
+
+import com.ayrtonto.nutriapi.model.entity.Exercise;
+import com.ayrtonto.nutriapi.repository.ExerciseRepository;
+import com.ayrtonto.nutriapi.service.AdminExerciseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class AdminExerciseServiceImpl implements AdminExerciseService {
+    private final ExerciseRepository exerciseRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Exercise> getAll() {
+        return exerciseRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Page<Exercise> paginate(Pageable pageable) {
+        return exerciseRepository.findAll(pageable);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Exercise findById(Integer id) {
+        return exerciseRepository.findById(id).orElseThrow(()->new RuntimeException("No exercise found with id: " + id));
+    }
+
+
+    @Transactional
+    @Override
+    public Exercise create(Exercise exercise) {
+        return exerciseRepository.save(exercise);
+    }
+
+    @Transactional
+    @Override
+    public Exercise update(Integer id, Exercise updateExercise) {
+        Exercise exercise = findById(id);
+        if (updateExercise.getDescription() != null) {
+            exercise.setDescription(updateExercise.getDescription());
+        }
+        if (updateExercise.getTips() != null) {
+            exercise.setTips(updateExercise.getTips());
+        }
+        if (updateExercise.getImage() != null) {
+            exercise.setImage(updateExercise.getImage());
+        }
+        if (updateExercise.getLinkVideo() != null) {
+            exercise.setLinkVideo(updateExercise.getLinkVideo());
+        }
+        return exerciseRepository.save(exercise);
+    }
+
+    @Transactional
+    @Override
+    public void delete(Integer id) {
+        Exercise exercise = findById(id);
+        exerciseRepository.delete(exercise);
+    }
+}
