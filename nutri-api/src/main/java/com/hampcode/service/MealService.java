a7c098a8649936f1ce3b838725fb0eaa057592d5
Luciano V. Achin Angeles <107821071+LuVaAcAn@users.noreply.github.com>
2024-09-16 13:02:54 -0500

    Update MealService.java

--- nutri-api/src/main/java/com/hampcode/service/MealService.java
+++ nutri-api/src/main/java/com/hampcode/service/MealService.java
@@ -1,23 +1,9 @@
 package com.hampcode.service;
 
 import com.hampcode.model.entity.Meal;
-import com.hampcode.repository.MealRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
-public class MealService {
-
-    @Autowired
-    private MealRepository mealRepository;
-
-    public List<Meal> findAllMeals() {
-        return mealRepository.findAll();
-    }
-
-    public Meal findMealById(Integer id) {
-        return mealRepository.findById(id).orElseThrow(() -> new RuntimeException("No se encuentra la comida"));
-    }
+public interface MealService {
+    List<Meal> findAllMeals();
+    Meal findMealById(Integer id);
 }
